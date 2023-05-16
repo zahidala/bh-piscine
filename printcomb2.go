0b0444bdc106c0449f72bd2ac89932ba05ff1088
zbutt <zahidalaaudin@gmail.com>
2023-05-16 22:06:56 +0300

    attempt

---
+++ printcomb2.go
@@ -0,0 +1,21 @@
+// package piscine
+
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb2() {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			z01.PrintRune(i)
+			z01.PrintRune(j)
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
+}
+
+func main() {
+	PrintComb2()
+}
