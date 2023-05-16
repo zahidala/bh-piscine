c484ff5f511757ce145120b2a208ff8d4099487a
zbutt <zahidalaaudin@gmail.com>
2023-05-16 16:51:50 +0300

    attempt to fix formatting issues

--- printcomb.go
+++ printcomb.go
@@ -1,38 +1,30 @@
-// package main
-package piscine
+package main
+// package piscine
 
 import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
-
-		for j := '1'; j <= '9'; j++ {
-
-			for k := '2'; k <= '9'; k++ {
-				
-				if ( i != j && j != k && i < j && j < k ) {
-
+		for j := i+1; j <= '9'; j++ {
+			for k := j+1; k <= '9'; k++ {
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
 
 				if (i == '7' && j == '8' && k == '9') {
-					
 					break
 				}
-
+				
 				z01.PrintRune(',')
 				z01.PrintRune(' ')
-
-				}
 			}
 		}
 	}
-		z01.PrintRune('\n')
+	z01.PrintRune('\n')
 }
 
 
 
-// func main() {
-// 	PrintComb()
-// }
+func main() {
+	PrintComb()
+}
