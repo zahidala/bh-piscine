fea779f7e02658b93350a600494fde439faa80f1
zbutt <zahidalaaudin@gmail.com>
2023-05-16 20:39:06 +0300

    still figuring it out

--- ultimatedivmod.go
+++ ultimatedivmod.go
@@ -1,28 +1,27 @@
-// package piscine
+package piscine
 
-package main
+// package main
 
 import "fmt"
 
 func UltimateDivMod(a *int, b *int) {
-	
-	fmt.Println("a before division:", *a)
+
+	// fmt.Println("a before division:", *a)
 	fmt.Println("b before division:", *b)
 
 	*a = *a / *b
 
 	fmt.Println("a after division:", *a)
 	fmt.Println("b after division:", *b)
-	
-	*b = *a % *b
 
+	*b = *b % *a
 
 }
 
-func main() {
-	a := 13
-	b := 2
-	UltimateDivMod(&a, &b)
-	fmt.Println(a)
-	fmt.Println(b)
-}
+// func main() {
+// 	a := 13
+// 	b := 2
+// 	UltimateDivMod(&a, &b)
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
