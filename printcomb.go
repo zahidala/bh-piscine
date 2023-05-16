ba116caea2388336cf3d9044e897448575fe5417
zbutt <zahidalaaudin@gmail.com>
2023-05-16 17:19:56 +0300

    remove rune cast

--- printcomb.go
+++ printcomb.go
@@ -7,9 +7,9 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := i + 1; j <= '9'; j++ {
 			for k := j + 1; k <= '9'; k++ {
-				z01.PrintRune(rune(i))
-				z01.PrintRune(rune(j))
-				z01.PrintRune(rune(k))
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 
 				if i == '7' && j == '8' && k == '9' {
 					break
