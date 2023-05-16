39b5da56f69ee7746d4f3b8eea32e5a3c212eb8e
zbutt <zahidalaaudin@gmail.com>
2023-05-16 16:45:26 +0300

    fix conditionals

--- printcomb.go
+++ printcomb.go
@@ -7,15 +7,28 @@ func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
 		for j := '1'; j <= '9'; j++ {
 			for k := '2'; k <= '9'; k++ {
+				
+				if ( i != j && j != k && i < j && j < k ) {
+
 				z01.PrintRune(rune(i))
 				z01.PrintRune(rune(j))
 				z01.PrintRune(rune(k))
-				z01.PrintRune('\n')
+
+				if (i == '7' && j == '8' && k == '9') {
+					break
+				}
+
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+
+				}
 			}
 		}
 	}
-	z01.PrintRune('\n')
+		z01.PrintRune('\n')
 }
+
+
 
 // func main() {
 // 	PrintComb()
