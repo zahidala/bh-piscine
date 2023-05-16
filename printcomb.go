18c1465f78d3e98b3745884535829650d27010b4
zbutt <zahidalaaudin@gmail.com>
2023-05-16 16:46:47 +0300

    attempt to fix format issues

--- printcomb.go
+++ printcomb.go
@@ -5,7 +5,9 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for i := '0'; i <= '9'; i++ {
+
 		for j := '1'; j <= '9'; j++ {
+
 			for k := '2'; k <= '9'; k++ {
 				
 				if ( i != j && j != k && i < j && j < k ) {
@@ -15,6 +17,7 @@ func PrintComb() {
 				z01.PrintRune(rune(k))
 
 				if (i == '7' && j == '8' && k == '9') {
+					
 					break
 				}
 
