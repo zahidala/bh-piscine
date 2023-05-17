ef5649d8d4c2672dba725cd9663503f1b7ad9492
zbutt <zahidalaaudin@gmail.com>
2023-05-17 17:47:51 +0300

    tested working

---
+++ swap.go
@@ -0,0 +1,24 @@
+package main
+
+// package piscine
+
+// import "fmt"
+
+func Swap(a *int, b *int) {
+
+	c := *a
+	d := *b
+
+	*a = d
+	*b = c
+
+
+}
+
+// func main() {
+// 	a := 0
+// 	b := 1
+// 	Swap(&a, &b)
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+// }
