2a59b4d9f6b90028a0cb1b4f2e14041fe5df33ee
zbutt <zahidalaaudin@gmail.com>
2023-05-17 17:33:17 +0300

    attempt

--- strlen.go
+++ strlen.go
@@ -1,13 +1,22 @@
-// package main
-package piscine
+package main
+// package piscine
 
-// import "fmt"
+import "fmt"
 
 func StrLen(s string) int {
-	return len(s)
+	
+	// count := 0
+
+	// for range s {
+	// 	count++
+	// }
+	// return count
+
+	s2 := []rune(s)
+	return len(s2)
 }
 
-// func main() {
-// 	l := StrLen("Hello World!")
-// 	fmt.Println(l)
-// }
+func main() {
+	l := StrLen("Hello World!")
+	fmt.Println(l)
+}
