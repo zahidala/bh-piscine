0db7b1af3cbd1727f9746248e658c6f6c4231981
zbutt <zahidalaaudin@gmail.com>
2023-05-17 16:45:54 +0300

    hope this works

--- printstr.go
+++ printstr.go
@@ -4,20 +4,24 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	z01.PrintRune(rune(s[0]))
-	z01.PrintRune(rune(s[1]))
-	z01.PrintRune(rune(s[2]))
-	z01.PrintRune(rune(s[3]))
-	z01.PrintRune(rune(s[4]))
-	z01.PrintRune(rune(s[5]))
-	z01.PrintRune(rune(s[6]))
-	z01.PrintRune(rune(s[7]))
-	z01.PrintRune(rune(s[8]))
-	z01.PrintRune(rune(s[9]))
-	z01.PrintRune(rune(s[10]))
-	z01.PrintRune(rune(s[11]))
+	// z01.PrintRune(rune(s[0]))
+	// z01.PrintRune(rune(s[1]))
+	// z01.PrintRune(rune(s[2]))
+	// z01.PrintRune(rune(s[3]))
+	// z01.PrintRune(rune(s[4]))
+	// z01.PrintRune(rune(s[5]))
+	// z01.PrintRune(rune(s[6]))
+	// z01.PrintRune(rune(s[7]))
+	// z01.PrintRune(rune(s[8]))
+	// z01.PrintRune(rune(s[9]))
+	// z01.PrintRune(rune(s[10]))
+	// z01.PrintRune(rune(s[11]))
+
+	for _, word := range s{
+		z01.PrintRune(word)
+	}
 }
 
-// func main() {
-// 	PrintStr("Hello World!")
-// }
+func main() {
+	PrintStr("Hello World!")
+}
