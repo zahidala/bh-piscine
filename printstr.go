0783e9ce8dc4e73f03ee4e72ec9d4f5bde46f145
zbutt <zahidalaaudin@gmail.com>
2023-05-17 16:13:09 +0300

    maybe works?

---
+++ printstr.go
@@ -0,0 +1,29 @@
+// package main
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintStr(s string) {
+	z01.PrintRune(rune(s[0]))
+	z01.PrintRune(rune(s[1]))
+	z01.PrintRune(rune(s[2]))
+	z01.PrintRune(rune(s[3]))
+	z01.PrintRune(rune(s[4]))
+	z01.PrintRune(rune(s[5]))
+	z01.PrintRune(rune(s[6]))
+	z01.PrintRune(rune(s[7]))
+	z01.PrintRune(rune(s[8]))
+	z01.PrintRune(rune(s[9]))
+	z01.PrintRune(rune(s[10]))
+	z01.PrintRune(rune(s[11]))
+	z01.PrintRune('\n')
+
+	// for i := '0'; i <= '9' + 2; i++ {
+	// 	// z01.PrintRune(rune(i))
+	// 	z01.PrintRune(rune(s[i]))
+	// }
+}
+
+// func main() {
+// 	PrintStr("Hello World!")
+// }
