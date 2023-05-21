ea1f83ad20c17a37dec1ca7dc477a1b5251a2ebb
zbutt <zahidalaaudin@gmail.com>
2023-05-21 21:18:20 +0300

    attempt

---
+++ nrune.go
@@ -0,0 +1,28 @@
+// package piscine
+
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func NRune(s string, n int) rune {
+	r := []rune(s)
+
+	if n == -1 {
+		return r[len(s)-1]
+	} else if n < len(s) {
+		return r[n-1]
+	} else {
+		return 0
+	}
+}
+func main() {
+	z01.PrintRune(NRune("Hello!", 3))
+	z01.PrintRune(NRune("Salut!", 2))
+	z01.PrintRune(NRune("Bye!", -1))
+	z01.PrintRune(NRune("Bye!", 5))
+	z01.PrintRune(NRune("Ola!", 4))
+	z01.PrintRune('\n')
+
+}
