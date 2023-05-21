61b96b2526aa248b1ceb7070ca8b58e9b9ed5f89
zbutt <zahidalaaudin@gmail.com>
2023-05-21 16:52:18 +0300

    working

---
+++ iterativepower.go
@@ -0,0 +1,19 @@
+package main
+
+// package piscine
+
+func IterativePower(nb int, power int) int {
+	if nb < 0 {
+		return 0
+	} else {
+		result := 1
+		for i := 1; i <= power; i++ {
+			result = result * nb
+		}
+		return result
+	}
+}
+
+// func main() {
+// 	fmt.Println(IterativePower(4, 3))
+// }
