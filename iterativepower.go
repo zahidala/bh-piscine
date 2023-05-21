33e803479fc5c321cb006fd1bbc3d7235e3a5180
zbutt <zahidalaaudin@gmail.com>
2023-05-21 16:58:00 +0300

    testing

--- iterativepower.go
+++ iterativepower.go
@@ -1,10 +1,12 @@
-// package main
+package main
 
-package piscine
+// package piscine
+
+import "fmt"
 
 func IterativePower(nb int, power int) int {
-	if nb < 0 {
-		return 0
+	if nb == -1 {
+		return -1
 	} else {
 		result := 1
 		for i := 1; i <= power; i++ {
@@ -14,6 +16,6 @@ func IterativePower(nb int, power int) int {
 	}
 }
 
-// func main() {
-// 	fmt.Println(IterativePower(4, 3))
-// }
+func main() {
+	fmt.Println(IterativePower(-4, 3))
+}
