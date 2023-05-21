5820c04dd2db41a572b1729fcd6bafd522cf8189
zbutt <zahidalaaudin@gmail.com>
2023-05-21 19:41:10 +0300

    done

---
+++ fibonacci.go
@@ -0,0 +1,24 @@
+package main
+
+// package piscine
+
+import "fmt"
+
+func Fibonacci(index int) int {
+
+	if index == 0 {
+		return 0
+	}
+
+	if index == 1 {
+		return 1
+	} else {
+		return Fibonacci(index-2) + Fibonacci(index-1)
+	}
+	return -1
+}
+
+func main() {
+	arg1 := 4
+	fmt.Println(Fibonacci(arg1))
+}
