5f06f28fe25a55505ffead4fdd6e8190e3a825e4
zbutt <zahidalaaudin@gmail.com>
2023-05-21 19:52:22 +0300

    fix conditionss

--- fibonacci.go
+++ fibonacci.go
@@ -9,12 +9,16 @@ func Fibonacci(index int) int {
 		return 0
 	}
 
+	if index < 0 {
+		return -1
+	}
+
 	if index == 1 {
 		return 1
 	} else {
 		return Fibonacci(index-2) + Fibonacci(index-1)
 	}
-	return -1
+
 }
 
 // func main() {
