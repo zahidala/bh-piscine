6f84ad2ea1af666db3ce71a04f228e4def73d136
zbutt <zahidalaaudin@gmail.com>
2023-05-21 21:34:47 +0300

    fix package issues

--- nrune.go
+++ nrune.go
@@ -1,8 +1,8 @@
-// package piscine
+package piscine
 
-package main
+// package main
 
-import "github.com/01-edu/z01"
+// import "github.com/01-edu/z01"
 
 func NRune(s string, n int) rune {
 	r := []rune(s)
@@ -16,11 +16,11 @@ func NRune(s string, n int) rune {
 	}
 }
 
-func main() {
-	z01.PrintRune(NRune("Hello!", 3))
-	z01.PrintRune(NRune("Salut!", 2))
-	z01.PrintRune(NRune("Bye!", -1))
-	z01.PrintRune(NRune("Bye!", 5))
-	z01.PrintRune(NRune("Ola!", 4))
-	z01.PrintRune('\n')
-}
+// func main() {
+// 	z01.PrintRune(NRune("Hello!", 3))
+// 	z01.PrintRune(NRune("Salut!", 2))
+// 	z01.PrintRune(NRune("Bye!", -1))
+// 	z01.PrintRune(NRune("Bye!", 5))
+// 	z01.PrintRune(NRune("Ola!", 4))
+// 	z01.PrintRune('\n')
+// }
