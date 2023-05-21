eeaaf61464a37eb4c55b0f45cea0544bc9e6e9a3
zbutt <zahidalaaudin@gmail.com>
2023-05-21 15:20:30 +0300

    attempt

---
+++ recursivefactorial.go
@@ -0,0 +1,19 @@
+// package main
+
+package piscine
+
+func RecursiveFactorial(nb int) int {
+
+	if nb == 1 {
+		return 1
+	}
+	if nb > 1 {
+		return nb * RecursiveFactorial(nb-1)
+	}
+	return 0
+}
+
+// func main() {
+// 	arg := 4
+// 	fmt.Println(RecursiveFactorial(arg))
+// }
