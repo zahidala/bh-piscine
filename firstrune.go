bc903578a21906e4308b0170cdd01f8c9c34e720
zbutt <zahidalaaudin@gmail.com>
2023-05-21 20:21:20 +0300

    attempt

---
+++ firstrune.go
@@ -0,0 +1,17 @@
+// package main
+
+package piscine
+
+// import "github.com/01-edu/z01"
+
+func FirstRune(s string) rune {
+	r := []rune(s)
+	return r[0]
+}
+
+// func main() {
+// 	z01.PrintRune(FirstRune("Hello!"))
+// 	z01.PrintRune(FirstRune("Salut!"))
+// 	z01.PrintRune(FirstRune("Ola!"))
+// 	z01.PrintRune('\n')
+// }
