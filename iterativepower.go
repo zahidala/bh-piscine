3efa19f11aacc3b1f58f2cb049ef4e82747cc1c5
zbutt <zahidalaaudin@gmail.com>
2023-05-21 17:26:52 +0300

    attempt to test with new conditions

--- iterativepower.go
+++ iterativepower.go
@@ -1,21 +1,24 @@
-// package main
+package main
 
-package piscine
+// package piscine
+
+import "fmt"
 
 func IterativePower(nb int, power int) int {
 	if nb == -1 {
 		return -1
-	} else if nb < 0 {
-		return 0
 	} else {
 		result := 1
 		for i := 1; i <= power; i++ {
 			result = result * nb
+			if result < 0 {
+				return 0
+			}
 		}
 		return result
 	}
 }
 
-// func main() {
-// 	fmt.Println(IterativePower(-1, -10))
-// }
+func main() {
+	fmt.Println(IterativePower(-1, 4))
+}
