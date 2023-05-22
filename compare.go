df662b443a18b314b1393f5c2638d0dae51eff92
zbutt <zahidalaaudin@gmail.com>
2023-05-22 15:41:27 +0300

    working

---
+++ compare.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+// package piscine
+
+func Compare(a, b string) int {
+
+	// for _, i := range a {
+	// 	for _, j := range b {
+	// 		if i == j {
+	// 			return 0
+	// 		} else if i > j {
+	// 			return 1
+	// 		} else {
+	// 			return -1
+	// 		}
+	// 	}
+	// }
+	// return 0
+
+	if a == b {
+		return 0
+	} else if a < b {
+		return -1
+	} else {
+		return 1
+	}
+
+}
+
+func main() {
+	fmt.Println(Compare("Hello!", "Hello!"))
+	fmt.Println(Compare("Salut!", "lut!"))
+	fmt.Println(Compare("Ola!", "Ol"))
+}
