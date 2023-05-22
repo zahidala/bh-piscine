6020313f85daac3dd22b3ef4ad6c5235983e0e21
zbutt <zahidalaaudin@gmail.com>
2023-05-22 17:01:44 +0300

    tested to work

--- alphacount.go
+++ alphacount.go
@@ -1,15 +1,34 @@
-package piscine
+// package piscine
 
-// package main
+package main
 
-import "unicode/utf8"
+import "fmt"
 
 func AlphaCount(s string) int {
-	return utf8.RuneCountInString(s)
+
+	alphaRune := []rune(s)
+
+	count := 0
+
+	for _, code := range alphaRune {
+		for i := 'a'; i <= 'z'; i++ {
+			if i == code {
+				count = count + 1
+			}
+		}
+
+		for j := 'A'; j <= 'Z'; j++ {
+			if j == code {
+				count = count + 1
+			}
+		}
+	}
+
+	return count
 }
 
-// func main() {
-// 	s := "Hello 78 World!    4455 /"
-// 	nb := AlphaCount(s)
-// 	fmt.Println(nb)
-// }
+func main() {
+	s := "Hello 78 World!    4455 /"
+	nb := AlphaCount(s)
+	fmt.Println(nb)
+}
