c390ecd2223c7ae95c92492aeb3d188ecabda407
zbutt <zahidalaaudin@gmail.com>
2023-05-23 20:32:36 +0300

    fix formatting issues

--- isalpha.go
+++ isalpha.go
@@ -5,14 +5,12 @@ package piscine
 // import "fmt"
 
 func IsAlpha(s string) bool {
-
 	for i := range s {
 		if (s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') && (s[i] < '0' || s[i] > '9') {
 			return false
 		}
 	}
 	return true
-
 }
 
 // func main() {
