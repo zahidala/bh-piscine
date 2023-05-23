686a9e0420e45225fc803795b9b5865dcc440f58
zbutt <zahidalaaudin@gmail.com>
2023-05-23 17:00:26 +0300

    tested

---
+++ islower.go
@@ -0,0 +1,19 @@
+package piscine
+
+// package main
+
+// import "fmt"
+
+func IsLower(s string) bool {
+	for i := range s {
+		if s[i] <= 'a' || s[i] >= 'z' {
+			return false
+		}
+	}
+	return true
+}
+
+// func main() {
+// 	fmt.Println(IsLower("hello"))
+// 	fmt.Println(IsLower("hello!"))
+// }
