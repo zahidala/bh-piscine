67fa352cc53d22c6777206d6b0199b4136036112
zbutt <zahidalaaudin@gmail.com>
2023-05-23 20:41:34 +0300

    done

---
+++ isnumeric.go
@@ -0,0 +1,19 @@
+// package piscine
+
+package main
+
+import "fmt"
+
+func IsNumeric(s string) bool {
+	for i := range s {
+		if (s[i] < '0' || s[i] > '9') {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(IsNumeric("010203"))
+	fmt.Println(IsNumeric("01,02,03"))
+}
