c252ac133dc8ca4b186e4f11fd28cc9b414a07fe
zbutt <zahidalaaudin@gmail.com>
2023-05-23 16:01:00 +0300

    works

---
+++ isupper.go
@@ -0,0 +1,21 @@
+// package piscine
+
+package main
+
+import "fmt"
+
+func IsUpper(s string) bool {
+	for i := range s {
+		if s[i] < 'A' || s[i] > 'Z' {
+			return false
+		}
+	}
+	return true
+}
+
+
+func main() {
+	fmt.Println(IsUpper("HELLO"))
+	fmt.Println(IsUpper("HELLO!"))
+
+}
