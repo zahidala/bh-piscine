0768d74e7c27b82c9a05a23976b4c75e90bdf7d3
zbutt <zahidalaaudin@gmail.com>
2023-05-24 17:34:43 +0300

    done

---
+++ printparams/main.go
@@ -0,0 +1,22 @@
+package main 
+
+import (
+	// "fmt"
+	"os"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	// arguments := os.Args
+	// for i := 1; i <= len(arguments)-1; i++ {
+	// 	fmt.Println((os.Args[i]))
+	// }
+
+	for i := 1; i<= len(os.Args)-1; i++ {
+		arguments := os.Args[i]
+		for _,value := range arguments {
+			z01.PrintRune(value)
+		}
+		z01.PrintRune('\n')
+	}
+}
