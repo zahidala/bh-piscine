a17bd199a927380b855c4f16a7a0680335c86296
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-29 22:55:22 +0300

    done

---
+++ map.go
@@ -0,0 +1,23 @@
+package piscine
+
+// package main
+
+import "fmt"
+
+func Map(f func(int) bool, a []int) []bool {
+	var s []bool
+	for _, v := range a {
+		if f(v) {
+			s = append(s, true)
+		} else {
+			s = append(s, false)
+		}
+	}
+	return s
+}
+
+func main() {
+	a := []int{1, 2, 3, 4, 5, 6}
+	result := Map(piscine.IsPrime, a)
+	fmt.Println(result)
+}
