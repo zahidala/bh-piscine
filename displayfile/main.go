feeae35d2c7e51fadc55247597b1a2cf5be8c6d9
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-29 21:43:26 +0300

    done

---
+++ displayfile/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("quest8.txt")
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else if len(os.Args) == 2 {
+		arr := make([]byte, 14)
+		file.Read(arr)
+		fmt.Println(string(arr))
+	}
+}
