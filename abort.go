e01d71a640d1391856bdc21625e1080566b3816b
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-05-31 22:19:35 +0300

    done

---
+++ abort.go
@@ -0,0 +1,12 @@
+// package piscine
+
+package main
+
+func Abort(a, b, c, d, e int) int {
+	return (a + b + c + d + e) / 5
+}
+
+// func main() {
+// 	middle := Abort(2, 3, 8, 5, 7)
+// 	fmt.Println(middle)
+// }
