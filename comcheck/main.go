c74341e829944446eaac6cc25e40748111d81bb6
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-01 15:57:25 +0300

    done

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	s := os.Args[1:]
 
 	for i := 0; i <= len(s)-1; i++ {
-		if s[i] == "01" {
+		if s[i] == "01" && s[i] == "galaxy" {
 			fmt.Println("Alert!!!")
 		} else if s[i] == "galaxy" {
 			fmt.Println("Alert!!!")
