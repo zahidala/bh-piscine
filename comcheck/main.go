0f069beb659f0e9893913c090d2383ccf050ce16
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-01 15:53:23 +0300

    done

---
+++ comcheck/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	s := os.Args[1:]
+
+	for i := 0; i <= len(s)-1; i++ {
+		if s[i] == "01" {
+			fmt.Println("Alert!!!")
+		} else if s[i] == "galaxy" {
+			fmt.Println("Alert!!!")
+		} else if s[i] == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
