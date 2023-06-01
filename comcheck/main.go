30fc5d605d754b432ccf603f2ef2fff26c4ad286
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-01 17:24:52 +0300

    attempt to fix conditions

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,11 +9,15 @@ func main() {
 	s := os.Args[1:]
 
 	for i := 0; i <= len(s)-1; i++ {
+		if s[i] == "01" {
+			fmt.Println("Alert!!!")
+		}
+
 		if s[i] == "01" && s[i] == "galaxy" {
-			fmt.Println("Alert!!!")
-		} else if s[i] == "galaxy" {
-			fmt.Println("Alert!!!")
-		} else if s[i] == "galaxy 01" {
+			fmt.Println("Alert")
+		}
+
+		if s[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
 		}
 	}
