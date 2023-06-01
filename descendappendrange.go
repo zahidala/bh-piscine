154ed9e5104aa31530e0416a90ca0ce4d3dd846d
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-01 17:53:19 +0300

    fix condition

--- descendappendrange.go
+++ descendappendrange.go
@@ -1,9 +1,15 @@
-package piscine
+// package piscine
 
-// package main
+package main
+
+import "fmt"
 
 func DescendAppendRange(max, min int) []int {
 	var numArray []int
+
+	if max <= min {
+		numArray = []int{}
+	}
 
 	for i := max - 1; i > min-1; i-- {
 		numArray = append(numArray, i+1)
@@ -11,7 +17,8 @@ func DescendAppendRange(max, min int) []int {
 	return numArray
 }
 
-// func main() {
-// 	fmt.Println(DescendAppendRange(10, 5))
-// 	fmt.Println(DescendAppendRange(5, 10))
-// }
+func main() {
+	fmt.Println(DescendAppendRange(10, 5))
+	fmt.Println(DescendAppendRange(5, 10))
+	fmt.Println(DescendAppendRange(0, 1))
+}
