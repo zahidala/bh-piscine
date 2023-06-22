c5598ff4cb37c024bf328449e6d24c46b248690f
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-22 20:40:05 +0300

    inital testing passed, now for the platform...

---
+++ listpushback.go
@@ -0,0 +1,43 @@
+package piscine
+
+// package main
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushBack(l *List, data interface{}) {
+	newNode := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = newNode
+		return
+	}
+
+	curr := l.Head
+	for curr.Next != nil {
+		curr = curr.Next
+	}
+
+	curr.Next = newNode
+}
+
+// func main() {
+
+// 	link := &List{}
+
+// 	ListPushBack(link, "Hello")
+// 	ListPushBack(link, "man")
+// 	ListPushBack(link, "how are you")
+
+// 	for link.Head != nil {
+// 		fmt.Println(link.Head.Data)
+// 		link.Head = link.Head.Next
+// 	}
+// }
