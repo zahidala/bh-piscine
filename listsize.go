66eec8704d6feea9e2a0bc6eb6e82d3cb875e42d
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-06-27 18:48:51 +0300

    fix redeclaration issue

--- listsize.go
+++ listsize.go
@@ -2,38 +2,38 @@ package piscine
 
 // package main
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
-func ListPushFront(l *List, data interface{}) {
-	newNode := &NodeL{Data: data}
+// func ListPushFront(l *List, data interface{}) {
+// 	newNode := &NodeL{Data: data}
 
-	if l.Head == nil {
-		l.Head = newNode
-		l.Tail = newNode
-		return
-	}
+// 	if l.Head == nil {
+// 		l.Head = newNode
+// 		l.Tail = newNode
+// 		return
+// 	}
 
-	newNode.Next = l.Head
-	l.Head = newNode
-}
+// 	newNode.Next = l.Head
+// 	l.Head = newNode
+// }
 
-func ListSize(l *List) int {
-	it := l.Head
-	count := 0
-	for it != nil {
-		count = count + 1
-		it = it.Next
-	}
-	return count
-}
+// func ListSize(l *List) int {
+// 	it := l.Head
+// 	count := 0
+// 	for it != nil {
+// 		count = count + 1
+// 		it = it.Next
+// 	}
+// 	return count
+// }
 
 // func main() {
 // 	link := &List{}
