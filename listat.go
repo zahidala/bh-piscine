5327166e6df896edd966bf6e712d3424fd22376e
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-01 19:20:11 +0300

    fix redeclaration error

--- listat.go
+++ listat.go
@@ -2,31 +2,31 @@ package piscine
 
 // package main
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
+// type NodeL struct {
+// 	Data interface{}
+// 	Next *NodeL
+// }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
+// type List struct {
+// 	Head *NodeL
+// 	Tail *NodeL
+// }
 
-func ListPushBack(l *List, data interface{}) {
-	newNode := &NodeL{Data: data}
+// func ListPushBack(l *List, data interface{}) {
+// 	newNode := &NodeL{Data: data}
 
-	if l.Head == nil {
-		l.Head = newNode
-		return
-	}
+// 	if l.Head == nil {
+// 		l.Head = newNode
+// 		return
+// 	}
 
-	curr := l.Head
-	for curr.Next != nil {
-		curr = curr.Next
-	}
+// 	curr := l.Head
+// 	for curr.Next != nil {
+// 		curr = curr.Next
+// 	}
 
-	curr.Next = newNode
-}
+// 	curr.Next = newNode
+// }
 
 func ListAt(l *NodeL, pos int) *NodeL {
 	it := l
