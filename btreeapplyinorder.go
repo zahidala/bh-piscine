c266a34f9d516042cd841ead7ba5ec61b2f2b934
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 16:22:24 +0300

    testing..

---
+++ btreeapplyinorder.go
@@ -0,0 +1,37 @@
+package piscine
+
+// package main
+
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
+
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 	}
+// 	return root
+// }
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root != nil {
+		BTreeApplyInorder(root.Left, f)
+		BTreeApplyInorder(root.Right, f)
+	}
+}
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	BTreeApplyInorder(root, fmt.Println)
+
+// }
