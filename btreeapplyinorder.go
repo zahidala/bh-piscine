90f23227aea6e8c6125e6dd368b741d332fde9e3
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 16:40:36 +0300

    inital tests passed

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -23,6 +23,7 @@ package piscine
 func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
 		BTreeApplyInorder(root.Left, f)
+		f(root.Data)
 		BTreeApplyInorder(root.Right, f)
 	}
 }
