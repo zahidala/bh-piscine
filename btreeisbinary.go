e7d785d26edac9bed4d74d4eed797923cc47abb7
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 18:25:07 +0300

    testing

---
+++ btreeisbinary.go
@@ -0,0 +1,43 @@
+package piscine
+
+// package main
+
+// type TreeNode struct {
+// 	Left, Right, Parent *TreeNode
+// 	Data                string
+// }
+
+// func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+// 	if root == nil {
+// 		return &TreeNode{Data: data}
+// 	}
+
+// 	if data < root.Data {
+// 		root.Left = BTreeInsertData(root.Left, data)
+// 		root.Left.Parent = root
+// 	} else {
+// 		root.Right = BTreeInsertData(root.Right, data)
+// 		root.Right.Parent = root
+// 	}
+// 	return root
+// }
+
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return false
+	}
+
+	if root.Left.Data < root.Data {
+		return true
+	} else {
+		return false
+	}
+}
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	fmt.Println(BTreeIsBinary(root))
+// }
