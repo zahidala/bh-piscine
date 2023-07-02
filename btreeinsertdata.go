eb59097fa85abd8ca1b1ff0d582e6f4ac8010fa2
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 15:37:48 +0300

    inital tests passed

---
+++ btreeinsertdata.go
@@ -0,0 +1,52 @@
+package piscine
+
+// package main
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	// if root == nil {
+	// 	return &TreeNode{Data: data}
+	// }
+
+	// if data < root.Data {
+	// 	root.Left = BTreeInsertData(root.Left, data)
+	// } else {
+	// 	root.Right = BTreeInsertData(root.Left, data)
+	// }
+	// return root
+
+	newNode := &TreeNode{Data: data}
+
+	if newNode.Data < root.Data {
+		if root.Left == nil {
+			root.Left = newNode
+		} else {
+			BTreeInsertData(root.Left, newNode.Data)
+		}
+	}
+
+	if newNode.Data > root.Data {
+		if root.Right == nil {
+			root.Right = newNode
+		} else {
+			BTreeInsertData(root.Right, newNode.Data)
+		}
+	}
+	return newNode
+}
+
+// func main() {
+// 	root := &TreeNode{Data: "4"}
+// 	BTreeInsertData(root, "1")
+// 	BTreeInsertData(root, "7")
+// 	BTreeInsertData(root, "5")
+// 	fmt.Println(root.Left.Data)
+// 	fmt.Println(root.Data)
+// 	fmt.Println(root.Right.Left.Data)
+// 	fmt.Println(root.Right.Data)
+
+// }
