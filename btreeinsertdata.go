07392aaacc38cc5d1ff2cc3d27677b8ed0f4ac75
Zahid Allaulddin <zahidalaauddin@gmail.com>
2023-07-02 15:49:44 +0300

    attempt to fix for test cases fail

--- btreeinsertdata.go
+++ btreeinsertdata.go
@@ -1,6 +1,8 @@
-package piscine
+// package piscine
 
-// package main
+package main
+
+import "fmt"
 
 type TreeNode struct {
 	Left, Right, Parent *TreeNode
@@ -8,47 +10,47 @@ type TreeNode struct {
 }
 
 func BTreeInsertData(root *TreeNode, data string) *TreeNode {
-	// if root == nil {
-	// 	return &TreeNode{Data: data}
-	// }
-
-	// if data < root.Data {
-	// 	root.Left = BTreeInsertData(root.Left, data)
-	// } else {
-	// 	root.Right = BTreeInsertData(root.Left, data)
-	// }
-	// return root
-
-	newNode := &TreeNode{Data: data}
-
-	if newNode.Data < root.Data {
-		if root.Left == nil {
-			root.Left = newNode
-		} else {
-			BTreeInsertData(root.Left, newNode.Data)
-			root.Left.Parent = root
-		}
+	if root == nil {
+		return &TreeNode{Data: data}
 	}
 
-	if newNode.Data > root.Data {
-		if root.Right == nil {
-			root.Right = newNode
-		} else {
-			BTreeInsertData(root.Right, newNode.Data)
-			root.Left.Parent = root
-		}
+	if data < root.Data {
+		root.Left = BTreeInsertData(root.Left, data)
+	} else {
+		root.Right = BTreeInsertData(root.Right, data)
 	}
-	return newNode
+	return root
+
+	// newNode := &TreeNode{Data: data}
+
+	// if newNode.Data < root.Data {
+	// 	if root.Left == nil {
+	// 		root.Left = newNode
+	// 	} else {
+	// 		BTreeInsertData(root.Left, newNode.Data)
+	// 		root.Left.Parent = root
+	// 	}
+	// }
+
+	// if newNode.Data > root.Data {
+	// 	if root.Right == nil {
+	// 		root.Right = newNode
+	// 	} else {
+	// 		BTreeInsertData(root.Right, newNode.Data)
+	// 		root.Left.Parent = root
+	// 	}
+	// }
+	// return newNode
 }
 
-// func main() {
-// 	root := &TreeNode{Data: "4"}
-// 	BTreeInsertData(root, "1")
-// 	BTreeInsertData(root, "7")
-// 	BTreeInsertData(root, "5")
-// 	fmt.Println(root.Left.Data)
-// 	fmt.Println(root.Data)
-// 	fmt.Println(root.Right.Left.Data)
-// 	fmt.Println(root.Right.Data)
+func main() {
+	root := &TreeNode{Data: "4"}
+	BTreeInsertData(root, "1")
+	BTreeInsertData(root, "7")
+	BTreeInsertData(root, "5")
+	fmt.Println(root.Left.Data)
+	fmt.Println(root.Data)
+	fmt.Println(root.Right.Left.Data)
+	fmt.Println(root.Right.Data)
 
-// }
+}
